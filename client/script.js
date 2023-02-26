c4240e66b5457cfcfbded799831f64a507d8822f
MuntasirArrib <mantasirahmed@gmail.com>
2023-02-26 12:15:57 +1100

    Main
    Main

--- client/script.js
+++ client/script.js
@@ -1,26 +1,27 @@
-import bot from './assets/bot.svg';
-import user from './assets/user.svg';
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
 
-const form = document.querySelector('form');
-const chatContainer =  document.querySelector('#chat_container');
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
 
-let loadInterval;
-
+let loadInterval
 
 function loader(element) {
-    element.textContent = '';
+    element.textContent = ''
 
     loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
         element.textContent += '.';
 
+        // If the loading indicator has reached three dots, reset it
         if (element.textContent === '....') {
             element.textContent = '';
         }
-    }, 300)
+    }, 300);
 }
 
 function typeText(element, text) {
-    let index = 0;
+    let index = 0
 
     let interval = setInterval(() => {
         if (index < text.length) {
@@ -32,6 +33,9 @@ function typeText(element, text) {
     }, 20)
 }
 
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
 function generateUniqueId() {
     const timestamp = Date.now();
     const randomNumber = Math.random();
@@ -40,75 +44,77 @@ function generateUniqueId() {
     return `id-${timestamp}-${hexadecimalString}`;
 }
 
-function chatStripe (isAi, value, uniqueId) {
+function chatStripe(isAi, value, uniqueId) {
     return (
         `
         <div class="wrapper ${isAi && 'ai'}">
             <div class="chat">
                 <div class="profile">
-                    <img
-                    src="${isAi ? bot : user}"
-                    alt="${isAi ? 'bot' : 'user'}"
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
                     />
                 </div>
                 <div class="message" id=${uniqueId}>${value}</div>
             </div>
         </div>
-        `
+    `
     )
 }
 
 const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = new FormData(form);
+    e.preventDefault()
 
-    //user's chatstrip
-    chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+    const data = new FormData(form)
 
-    form.reset();
+    // user's chatstripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
 
-    //bot's chatstripe
-    const uniqueId = generateUniqueId();
-    chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+    // to clear the textarea input 
+    form.reset()
 
+    // bot's chatstripe
+    const uniqueId = generateUniqueId()
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+    // to focus scroll to the bottom 
     chatContainer.scrollTop = chatContainer.scrollHeight;
 
-    const messageDiv = document.getElementById(uniqueId);
+    // specific message div 
+    const messageDiv = document.getElementById(uniqueId)
 
-    loader(messageDiv);
+    // messageDiv.innerHTML = "..."
+    loader(messageDiv)
 
-    // fetch data from server -> bot's response
-    debugger
-    const response = await fetch ('http://localhost:4000', {
+    const response = await fetch('http://localhost:3000', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
         },
         body: JSON.stringify({
             prompt: data.get('prompt')
         })
     })
 
-    clearInterval(loadInterval);
-    messageDiv.innerHTML = '';
+    clearInterval(loadInterval)
+    messageDiv.innerHTML = " "
 
     if (response.ok) {
         const data = await response.json();
-        const parsedData = data.bot.trim();
+        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
 
-        typeText(messageDiv, parsedData);
+        typeText(messageDiv, parsedData)
     } else {
-        const err = await response.text();
+        const err = await response.text()
 
-        messageDiv.innerHTML = "Something went wrong";
-
-        alert(err);
-    }   
+        messageDiv.innerHTML = "Something went wrong"
+        alert(err)
+    }
 }
 
-form.addEventListener('submit', handleSubmit);
+form.addEventListener('submit', handleSubmit)
 form.addEventListener('keyup', (e) => {
     if (e.keyCode === 13) {
-        handleSubmit(e);
+        handleSubmit(e)
     }
 })
